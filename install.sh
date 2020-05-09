830efd364c9050b8d9cebb4cd1045b125089c00d
marienvo <marien.vanoverbeek@weareyou.com>
2020-05-09 16:15:16 +0200

    Adds neovim init file

--- install.sh
+++ install.sh
@@ -3,9 +3,12 @@ rm ~/.config/alacritty/alacritty.yml
 rm ~/.zshrc
 rm ~/.vimrc
 rm ~/.tmux.conf
+rm -rf ~/.config/nvim
 
 # Create symlinks
 ln -s ~/.dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+mkdir ~/.config/nvim
+ln -s ~/.dotfiles/init.vim ~/.config/nvim/init.vim
