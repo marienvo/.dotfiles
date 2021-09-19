085fef3d7dbafbfe83be28f91a3b1de2c9fa654f
marienvo <marien.vanoverbeek@weareyou.com>
2021-09-19 21:38:02 +0200

    Adds mkdir

--- install.sh
+++ install.sh
@@ -12,15 +12,20 @@ rm -rf ~/.config/nvim
 # Create symlinks
 ln -s ~/.dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -s ~/.dotfiles/config.fish ~/.config/fish/config.fish
+
+mkdir ~/.config/newsboat
 ln -s ~/.dotfiles/newsboat/config ~/.config/newsboat/config
 ln -s ~/.dotfiles/newsboat/urls ~/.config/newsboat/urls
+
 mkdir ~/.moc/themes
 ln -s ~/.dotfiles/.moc/themes/default ~/.moc/themes/default
 ln -s ~/.dotfiles/.moc/config ~/.moc/config
+
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.taskrc ~/.taskrc
 ln -s ~/.dotfiles/.ideavimrc ~/.ideavimrc
+
 mkdir ~/.config/nvim
 ln -s ~/.dotfiles/init.vim ~/.config/nvim/init.vim
