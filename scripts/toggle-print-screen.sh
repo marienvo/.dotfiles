fe32314266ca5e56699455ed5665965da2a40ff7
Marien van Overbeek <git@marienvanoverbeek.nl>
2021-09-25 13:32:57 +0200

    Adds Peek for default print screen

--- scripts/toggle-print-screen.sh
+++ scripts/toggle-print-screen.sh
@@ -1,28 +1,8 @@
 #!/bin/bash
-######################################################################################################
-# This script will toggle minimize/activate first window with specified class
-# If window not found program will be launched
-#
-# window class can be found with next programs:
-#   wmctrl -x -l
-#   xprop
-#
-#
-# **IMPORTANT**
-# No credit taken.......... Cannot read the original.....
-# Found on http://blog.sokolov.me/2014/06/20/linuxx11-toggle-window-minimizemaximize/
-# in Russian :) but works when adjusting the wrapping.
-# Assigned to meta-f in KDE plasma 5
-######################################################################################################
-
-ps cax|grep alacritty
+ps cax|grep peek
 if [ $? -eq 0 ]; then
-  PID=`ps cax|grep alacritty | grep -o '^[ ]*[0-9]*'`
+  PID=`ps cax|grep peek | grep -o '^[ ]*[0-9]*'`
   echo "Process is already running [$PID]"
-  kill -1 $PID && alacritty
 else
-  alacritty
+  peek
 fi
-
-# todo: uitzoeken waarom wmctrl niet lekker werkt met Firefox, Alacritty? Iets met wayland of 64 bit vs 32?
-# for now use alt+Z: https://extensions.gnome.org/extension/3942/toggle-alacritty/
